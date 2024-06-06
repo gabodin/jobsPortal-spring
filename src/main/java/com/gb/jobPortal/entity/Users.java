47eb85567a399d8de2f941e4adb89eefd31125d9
gabodin <vgabriel2211@gmail.com>
2024-06-06 10:58:19 -0300

    Adding HomeController, templates and initial entities Users and UsersType

---
+++ src/main/java/com/gb/jobPortal/entity/Users.java
@@ -0,0 +1,104 @@
+package com.gb.jobPortal.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private boolean isActive;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+    private UsersType usersType;
+
+    public Users() {
+
+    }
+
+    public Users(Integer id, String email, String password, boolean isActive, Date registrationDate, UsersType usersType) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+        this.registrationDate = registrationDate;
+        this.usersType = usersType;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public @NotEmpty String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotEmpty String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public UsersType getUsersType() {
+        return usersType;
+    }
+
+    public void setUsersType(UsersType usersType) {
+        this.usersType = usersType;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
+                ", registrationDate=" + registrationDate +
+                ", usersType=" + usersType +
+                '}';
+    }
+}
---
+++ src/main/java/com/gb/jobPortal/entity/UsersType.java
@@ -0,0 +1,62 @@
+package com.gb.jobPortal.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users_type")
+public class UsersType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userTypeId;
+
+    private String userTypeName;
+
+    @OneToMany(targetEntity = Users.class, mappedBy = "userTypeId", cascade = CascadeType.ALL)
+    private List<Users> users;
+
+    public UsersType() {
+
+    }
+
+    public UsersType(Integer userTypeId, String userTypeName, List<Users> users) {
+        this.userTypeId = userTypeId;
+        this.userTypeName = userTypeName;
+        this.users = users;
+    }
+
+    public Integer getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(Integer userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    public List<Users> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Users> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersType{" +
+                "userTypeId=" + userTypeId +
+                ", userTypeName='" + userTypeName + '\'' +
+                ", users=" + users +
+                '}';
+    }
+}
