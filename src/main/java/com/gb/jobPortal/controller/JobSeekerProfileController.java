37bec628e4653bd345804691483bc81929cc1f58
gabodin <vgabriel2211@gmail.com>
2024-07-23 11:29:54 -0300

    feat: finishing persisting jobSeekerProfile

--- src/main/java/com/gb/jobPortal/controller/JobSeekerProfileController.java
+++ src/main/java/com/gb/jobPortal/controller/JobSeekerProfileController.java
@@ -6,6 +6,7 @@ import com.gb.jobPortal.entity.Skills;
 import com.gb.jobPortal.entity.Users;
 import com.gb.jobPortal.repository.UsersRepository;
 import com.gb.jobPortal.services.JobSeekerProfileService;
+import com.gb.jobPortal.utils.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,13 +14,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -65,9 +70,9 @@ public class JobSeekerProfileController {
         return "job-seeker-profile";
     }
 
-    @GetMapping("/addNew")
+    @PostMapping("/addNew")
     public String addJobSeekerProfile(JobSeekerProfile jobSeekerProfile,
-                                      @RequestParam("image") MultipartFile file,
+                                      @RequestParam("image") MultipartFile image,
                                       @RequestParam("pdf") MultipartFile pdf,
                                       Model model) {
 
@@ -80,8 +85,37 @@ public class JobSeekerProfileController {
             jobSeekerProfile.setUserAccountId(user.getUserId());
         }
 
-        model.addAttribute("profile", jobSeekerProfile);
+        List<Skills> skills = new ArrayList<>();
 
+        jobSeekerProfile.getSkills().forEach(skill -> {
+            skill.setJobSeekerProfile(jobSeekerProfile);
+        });
+
+        String imageName = "";
+        String resumeName = "";
+
+        if(!(Objects.equals(image.getOriginalFilename(), ""))) {
+            imageName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
+            jobSeekerProfile.setProfilePhoto(imageName);
+        }
+
+        if(!(Objects.equals(pdf.getOriginalFilename(), ""))) {
+            resumeName = StringUtils.cleanPath(Objects.requireNonNull(pdf.getOriginalFilename()));
+            jobSeekerProfile.setResume(resumeName);
+        }
+
+        JobSeekerProfile jobSeekerSaved = jobSeekerProfileService.addNew(jobSeekerProfile);
+
+        try {
+            String uploadDir = "photos/candidate/" + jobSeekerSaved.getUserAccountId() + "/";
+            FileUploadUtil.saveFile(uploadDir, imageName, image);
+            FileUploadUtil.saveFile(uploadDir, resumeName, pdf);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        model.addAttribute("profile", jobSeekerProfile);
+        model.addAttribute("skills", skills);
 
         return "redirect:/dashboard/";
     }
